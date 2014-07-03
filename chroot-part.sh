24df9fd596d581a70a7511469e3cceb2555f287e
Kristaps Kaupe <kristaps@blogiem.lv>
2014-07-03 23:41:45 +0300

    Cleanup at the end.

--- chroot-part.sh
+++ chroot-part.sh
@@ -241,5 +241,8 @@ case $BOOTLOADER in
     ;;
 esac
 
-echo "-- All DONE, reboot to finish."
+echo "--- Cleanup"
+rm -f /stage3-*.tar.bz2 /chroot-part.sh /inc.config.sh /mounts.txt /use_dhcpcd.txt
+
+echo "--- All DONE, reboot to finish."
 
