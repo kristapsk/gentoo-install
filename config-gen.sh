03adf9ebe0c07719d8385aceaf980857d4b2267c
Kristaps Kaupe <kristaps.k@intelligentsystems.lv>
2016-06-10 16:28:59 +0300

    Better stderr filtering

--- config-gen.sh
+++ config-gen.sh
@@ -86,7 +86,7 @@ else
 fi
 echo "KERNEL_EXTRA_FIRMWARE=\"$KERNEL_EXTRA_FIRMWARE\"" >> "$outfile"
 # Don't output error on not having IPv6 stable secret, see http://unix.stackexchange.com/a/255955/151693
-SYSCTL_ALL="`sysctl -a 2>&1 | grep -ve 'sysctl: reading key "net.ipv6.conf.*.stable_secret"'`"
+SYSCTL_ALL="`sysctl -a 2> >(grep -ve 'sysctl: reading key "net.ipv6.conf.*.stable_secret"' >&2)`"
 SYSCTL_SYSTEM="`sysctl --system | grep -v "* Applying" | sed -e 's/^/\\t/'`"
 echo "SYSCTL_CONF=\"" >> "$outfile"
 echo "$SYSCTL_SYSTEM" | while read sysctl_line; do
